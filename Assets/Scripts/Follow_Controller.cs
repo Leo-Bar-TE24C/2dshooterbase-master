4bd6b786042bcf69603a0a3aad0f8e273f11b078
Leo-Bar-TE24C <leonardo.barajasrios@elev.ga.ntig.se>
2025-11-05 09:47:51 +0100

    same as last time

--- Assets/Scripts/Follow_Controller.cs
+++ Assets/Scripts/Follow_Controller.cs
@@ -29,11 +29,27 @@ public class Follow_Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        bool goUp= Physics2D.OverlapCircle(up.transform.position, .3f, playerLayer);
-        bool goLeft= Physics2D.OverlapCircle(left.transform.position, .3f, playerLayer);
-        bool goRight= Physics2D.OverlapCircle(right.transform.position, .3f, playerLayer);
-        bool goDown= Physics2D.OverlapCircle(down.transform.position, .3f, playerLayer);
+        bool goUp = Physics2D.OverlapCircle(up.transform.position, .3f, playerLayer);
+        bool goLeft = Physics2D.OverlapCircle(left.transform.position, .3f, playerLayer);
+        bool goRight = Physics2D.OverlapCircle(right.transform.position, .3f, playerLayer);
+        bool goDown = Physics2D.OverlapCircle(down.transform.position, .3f, playerLayer);
 
+        while (goRight == true)
+        {
+            
+        }
+        while (goLeft == true)
+        {
+            
+        }
+        while (goUp == true)
+        {
+            
+        }
+        while (goDown == true)
+        {
+            
+        }
     }
 
 }
