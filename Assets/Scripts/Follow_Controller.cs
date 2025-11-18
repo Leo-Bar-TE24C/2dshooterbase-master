01e609617fa74beb1b73ca34556509642272699f
Leo-Bar-TE24C <leonardo.barajasrios@elev.ga.ntig.se>
2025-11-18 16:23:18 +0100

    worked on level design (and gave up on my overcomplicated camera follow code)

--- Assets/Scripts/Follow_Controller.cs
+++ Assets/Scripts/Follow_Controller.cs
@@ -2,17 +2,7 @@ using UnityEngine;
 
 public class Follow_Controller : MonoBehaviour
 {
-    [SerializeField]
-    GameObject up;
-
-    [SerializeField]
-    GameObject left;
-
-    [SerializeField]
-    GameObject right;
-
-    [SerializeField]
-    GameObject down;
+    
 
 
     [SerializeField]
@@ -22,34 +12,12 @@ public class Follow_Controller : MonoBehaviour
     LayerMask playerLayer;
     void Start()
     {
-        Vector3 pos = transform.position;
-        pos.x = target.transform.position.x;
-        transform.position = pos;
+       
     }
     // Update is called once per frame
     void Update()
     {
-        bool goUp = Physics2D.OverlapCircle(up.transform.position, .3f, playerLayer);
-        bool goLeft = Physics2D.OverlapCircle(left.transform.position, .3f, playerLayer);
-        bool goRight = Physics2D.OverlapCircle(right.transform.position, .3f, playerLayer);
-        bool goDown = Physics2D.OverlapCircle(down.transform.position, .3f, playerLayer);
-
-        while (goRight == true)
-        {
-            
-        }
-        while (goLeft == true)
-        {
-            
-        }
-        while (goUp == true)
-        {
-            
-        }
-        while (goDown == true)
-        {
-            
-        }
+        
     }
 
 }
